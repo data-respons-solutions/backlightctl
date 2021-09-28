1c6e8e8f8490838b9d150875fafde2a69777e025
Mikko Salomäki <ms@datarespons.se>
2021-09-28 20:26:51 +0200

    libbacklight: fully support trigger

--- test-libbacklight.cpp
+++ test-libbacklight.cpp
@@ -2,26 +2,10 @@
 #include <cstdint>
 #include <cerrno>
 #include <ctime>
+#include <cstdio>
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
-
-// bitwise operators for enum class flags
-template <typename Enumeration, typename = typename std::enable_if<std::is_enum<Enumeration>::value, Enumeration>::type>
-Enumeration operator&(Enumeration a, Enumeration b)
-{return static_cast<Enumeration>(static_cast<typename std::underlying_type<Enumeration>::type>(a) & static_cast<typename std::underlying_type<Enumeration>::type>(b));}
-template <typename Enumeration, typename = typename std::enable_if<std::is_enum<Enumeration>::value, Enumeration>::type>
-Enumeration operator|(Enumeration a, Enumeration b)
-{return static_cast<Enumeration>(static_cast<typename std::underlying_type<Enumeration>::type>(a) | static_cast<typename std::underlying_type<Enumeration>::type>(b));}
-template <typename Enumeration, typename = typename std::enable_if<std::is_enum<Enumeration>::value, Enumeration>::type>
-Enumeration& operator&=(Enumeration& a, Enumeration b)
-{return a = a & b;}
-template <typename Enumeration, typename = typename std::enable_if<std::is_enum<Enumeration>::value, Enumeration>::type>
-Enumeration& operator|=(Enumeration& a, Enumeration b)
-{return a = a | b;}
-template <typename Enumeration, typename = typename std::enable_if<std::is_enum<Enumeration>::value, Enumeration>::type>
-Enumeration& operator~(Enumeration a)
-{return ~static_cast<Enumeration>(static_cast<typename std::underlying_type<Enumeration>::type>(a));}
 
 struct libbacklight_conf {
 	uint32_t max_brightness_step;
@@ -38,6 +22,8 @@ struct libbacklight_ctrl {
 
 int libbacklight_init(struct libbacklight_ctrl* ctrl, const struct timespec* ts, const struct libbacklight_conf* conf)
 {
+	memset(ctrl, 0, sizeof(struct libbacklight_ctrl));
+
 	if (conf->max_brightness_step == 0)
 		return -EINVAL;
 
@@ -55,8 +41,8 @@ int libbacklight_init(struct libbacklight_ctrl* ctrl, const struct timespec* ts,
 }
 
 enum libbacklight_action {
-	LIBBACKLIGHT_NONE =			0,
-	LIBBACKLIGHT_BRIGHTNESS = 	1 << 0,
+	LIBBACKLIGHT_NONE,
+	LIBBACKLIGHT_BRIGHTNESS,
 };
 
 static struct timespec timespec_sub(const struct timespec* ts1, const struct timespec* ts2)
@@ -77,7 +63,6 @@ static int timespec_cmp(const struct timespec* ts1, const struct timespec* ts2)
 			return 0;
 		return ts1->tv_nsec > ts2->tv_nsec ? 1 : -1;
 	}
-	printf("ts1: %d: ts2: %d\n", ts1->tv_sec, ts2->tv_sec);
 	return ts1->tv_sec > ts2->tv_sec ? 1 : -1;
 }
 
@@ -85,17 +70,26 @@ enum libbacklight_action libbacklight_operate(struct libbacklight_ctrl* ctrl, co
 {
 	enum libbacklight_action ac = LIBBACKLIGHT_NONE;
 
-	if (ctrl->conf.enable_trigger && ctrl->brightness_step > 0) {
-		const struct timespec since_last = timespec_sub(&ctrl->last_trigger, ts);
-		if (timespec_cmp(&ctrl->conf.trigger_timeout, &since_last) <= 0) {
-			ac |= LIBBACKLIGHT_BRIGHTNESS;
-			ctrl->brightness_step = 0;
+	if (ctrl->conf.enable_trigger) {
+		if (triggered) {
+			memcpy(&ctrl->last_trigger, ts, sizeof(struct timespec));
+			if (ctrl->brightness_step == 0) {
+				ctrl->brightness_step = ctrl->conf.initial_brightness_step;
+				ac = LIBBACKLIGHT_BRIGHTNESS;
+			}
+		}
+		else
+		if (ctrl->brightness_step > 0) {
+			const struct timespec since_last = timespec_sub(&ctrl->last_trigger, ts);
+			if (timespec_cmp(&ctrl->conf.trigger_timeout, &since_last) <= 0) {
+				ctrl->brightness_step = 0;
+				ac = LIBBACKLIGHT_BRIGHTNESS;
+			}
 		}
 	}
-	(void) ts;
+
 	(void) triggered;
 	(void) lux;
-	(void) ctrl;
 
 	return ac;
 }
@@ -107,25 +101,26 @@ TEST_CASE("libbacklight_init simple") {
 	conf.initial_brightness_step = 5;
 
 	struct libbacklight_ctrl ctrl;
-	struct timespec ts;
+	struct timespec ts = {0,0};
 	int r = libbacklight_init(&ctrl, &ts, &conf);
 	REQUIRE(r == 0);
 }
 
 TEST_CASE("Test trigger") {
-	SECTION("Timeout") {
-		struct libbacklight_conf conf;
-		conf.max_brightness_step = 10;
-		conf.initial_brightness_step = 5;
-		conf.enable_trigger = 1;
-		conf.trigger_timeout.tv_sec = 10;
-		struct libbacklight_ctrl ctrl;
-		struct timespec ts;
-		int r = libbacklight_init(&ctrl, &ts, &conf);
-		REQUIRE(r == 0);
+	struct libbacklight_conf conf;
+	conf.max_brightness_step = 10;
+	conf.initial_brightness_step = 5;
+	conf.enable_trigger = 1;
+	conf.trigger_timeout.tv_sec = 10;
+	conf.trigger_timeout.tv_nsec = 0;
+	struct libbacklight_ctrl ctrl;
+	struct timespec ts = {0,0};
+	int r = libbacklight_init(&ctrl, &ts, &conf);
+	REQUIRE(r == 0);
 
+	SECTION("Timeout") {
 		enum libbacklight_action ac = LIBBACKLIGHT_NONE;
-		for (int i = 0; i < 9; ++i) {
+		for (int i = 0; i < 10; ++i) {
 			ts.tv_sec = i;
 			ac = libbacklight_operate(&ctrl, &ts, 0, 0);
 			REQUIRE(ac == LIBBACKLIGHT_NONE);
@@ -135,7 +130,17 @@ TEST_CASE("Test trigger") {
 		REQUIRE(ac == LIBBACKLIGHT_BRIGHTNESS);
 		REQUIRE(ctrl.brightness_step == 0);
 
-		// Section -> Test re-enable backlight
+		SECTION("Trigger") {
+			ac = libbacklight_operate(&ctrl, &ts, 1, 0);
+			REQUIRE(ac == LIBBACKLIGHT_BRIGHTNESS);
+			REQUIRE(ctrl.brightness_step == conf.initial_brightness_step);
+
+			SECTION("Timeout again")
+				ts.tv_sec = 21;
+				ac = libbacklight_operate(&ctrl, &ts, 0, 0);
+				REQUIRE(ac == LIBBACKLIGHT_BRIGHTNESS);
+				REQUIRE(ctrl.brightness_step == 0);
+		}
 	}
 }
 
